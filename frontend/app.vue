dda72691c9ec1ab2add45ba9f3efc26e5e001058
selim <selimdev00@gmail.com>
2023-03-16 11:31:47 +0500

    [init]: init nuxt & init strapi & intgeration between them

---
+++ frontend/app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { Landing } from "~/types";
+
+const { find } = useStrapi();
+
+const { data, pending, refresh, error } = await useAsyncData(
+  "landing",
+  async () => {
+    const response = await find<Landing>("landing");
+
+    return response?.data?.attributes;
+  }
+);
+</script>
+
+<template>
+  <div>
+    <h1>{{ data.title }}</h1>
+  </div>
+</template>
---
+++ frontend/types/index.ts
@@ -0,0 +1,8 @@
+export interface Post {
+    id: number;
+    title: string;
+}
+
+export interface Landing {
+    title: string;
+}
